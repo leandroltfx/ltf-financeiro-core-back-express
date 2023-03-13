6a81c56ac097e93a44d40bd4aca01002215c6f27
leandrobr95 <leandrobsb55@gmail.com>
2023-03-12 22:15:42 -0300

    alterando tecnologia para o nestjs

--- src/config/db-connect.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.set('strictQuery', false);
-
-mongoose.connect('mongodb+srv://leandroltfx:asd123@cluster0.wyld4ap.mongodb.net/ltf-financeiro-database-mongo');
-
-export default mongoose.connection;
