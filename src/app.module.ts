3f717f639052cfed2980799e0adfee221cae872f
leandrobr95 <leandrobsb55@gmail.com>
2023-03-12 23:59:27 -0300

    instalando e configurando mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://leandroltfx:asd123@cluster0.wyld4ap.mongodb.net/ltf-financeiro-database-mongo')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
